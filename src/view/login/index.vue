64dab63d226a03cc7d6238535c9c8c6188cf7f0d
A190203652 <1303657441@qq.com>
2019-07-15 11:32:12 +0800

    Update index.vue
    页面构建完成

--- src/view/login/index.vue
+++ src/view/login/index.vue
@@ -1,7 +1,23 @@
 <template>
   <div class="login-container">
     <el-card class="login-card">
-<img src="../../assets/images/logo_index.png" alt="">
+      <img src="../../assets/images/logo_index.png" alt />
+
+      <el-form rules="rules" ref="loginForm" :model="loginForm">
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-input v-model="loginForm" placeholder="请输入验证码" style="width:140px"></el-input>
+          <el-button style="float:right" type="primary">验证信息</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox :value="true" style="margin-bottom:0">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width:100%" @click="login()">登录</el-button>
+        </el-form-item>
+      </el-form>
     </el-card>
   </div>
 </template>
@@ -13,18 +29,22 @@ export default {}
 <style lang="less" scoped>
 .login-container {
   position: absolute;
-  background:url(../../assets/images/login_bg.jpg) no-repeat  center / cover;
+  background: url(../../assets/images/login_bg.jpg) no-repeat center / cover;
   width: 100%;
   height: 100%;
   top: 0;
   left: 0;
 }
 .login-card {
-position: absolute;
-top: 50%;
-left: 50%;
-transform: translate(-50%,-50%);
-width: 300px;
-height: 300px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 300px;
+  height: 300px;
+}
+img {
+  width: 200px;
+  height: 40px;
 }
 </style>
